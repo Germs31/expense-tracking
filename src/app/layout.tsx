fcb41fb4cfa69e24fe5648a795cfbf631c657dde
Germs31 <german90nunez@gmail.com>
2025-05-28 16:12:21 -0700

    ugly layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SideBar from "@/components/Sidebar/SideBar";
+import Header from "@/components/Header/Header";
 
 export const metadata: Metadata = {
   title: "Active Expenses",
@@ -14,7 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Header/>
+        <div className="flex">
+          <SideBar/>
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-screen flex justify-around items-center bg-amber-400 h-14 text-white p-8:">
+      <div>Expense Tracker</div>
+      <div>Sign In</div>
+    </div>
+  )
+}
+
+export default Header
