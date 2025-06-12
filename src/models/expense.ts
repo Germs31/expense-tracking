f0caf782df2408cb1b1ead24b2bb42aaa1d2663e
Germs31 <german90nunez@gmail.com>
2025-06-11 17:31:18 -0700

    updated models to prevent linting error, more straightforward

--- src/models/expense.ts
+++ src/models/expense.ts
@@ -1,26 +1,25 @@
-import { Schema, model, models, Document, Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 
-export interface IExpense extends Document {
-    title: string;
-    amount: number;
-    category: string;
-    dueDate: Date;
-    notes?: string;
-    createdAt: Date;
-    user: Types.ObjectId | string; // Reference to the user who owns this expense
+export interface Expense {
+  title: string;
+  amount: number;
+  category: string;
+  dueDate: Date;
+  notes?: string;
+  createdAt: Date;
+  user: Types.ObjectId | string; // Reference to the user who owns this expense
 }
 
-const expenseSchema: Schema<IExpense> = new Schema({
+const schema = new mongoose.Schema<Expense>(
+  {
     title: { type: String, required: true },
     amount: { type: Number, required: true },
     category: { type: String, required: true },
     dueDate: { type: Date, required: false },
     notes: { type: String },
     createdAt: { type: Date, default: Date.now },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true } // Reference to User document
-});
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // Reference to User document
+  }
+);
 
-// Use existing model if it exists, otherwise create a new one
-const Expense = models.Expense || model<IExpense>('Expense', expenseSchema);
-
-export default Expense;
+export default (mongoose.models.Expense as mongoose.Model<Expense>) || mongoose.model<Expense>('Expense', schema);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,18 @@
-import { Schema, model, models, Document, Types } from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 
-export interface IUser extends Document {
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-    address: string;
-    createdAt: Date;
-    lastUpdated: Date;
-    monthlyIncome: string;
-    expenses: Types.ObjectId[] | string[]; // Reference to expenses
+export interface User {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  address: string;
+  createdAt: Date;
+  lastUpdated: Date;
+  monthlyIncome: string;
+  expenses: Types.ObjectId[] | string[]; // Reference to expenses
 }
 
-const userSchema: Schema<IUser> = new Schema({
+const schema = new mongoose.Schema<User>(
+  {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     phoneNumber: { type: String, required: true },
@@ -19,10 +20,8 @@ const userSchema: Schema<IUser> = new Schema({
     createdAt: { type: Date, default: Date.now },
     lastUpdated: { type: Date, default: Date.now },
     monthlyIncome: { type: String, required: true },
-    expenses: [{ type: Schema.Types.ObjectId, ref: 'Expense', required: true}] // Array of references to Expense documents
-});
+    expenses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Expense', required: true }] // Array of references to Expense documents
+  }
+);
 
-// Use existing model if it exists, otherwise create a new one
-const User = models.User || model<IUser>('User', userSchema);
-
-export default User;
+export default (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>('User', schema);
